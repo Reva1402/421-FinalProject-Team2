b4b4d8ecf06e16bbdf1892c4c55932c09752686e
Reva1402 <148901721+Reva1402@users.noreply.github.com>
2024-11-04 23:39:41 -0500

    Update WelcomePage.js

--- WelcomePage.js
+++ WelcomePage.js
@@ -18,25 +18,76 @@ const SignupPage = () => {
         address: '',
         country: '',
         province: ''
-        
-    });
 
+    });
+    const [provinces, setProvinces] = useState([]);
     const [error, setError] = useState('');
+
+
 
     const navigate = useNavigate();
 
+
+
+
+
+
     const handleInputChange = (e) => {
         const { name, value } = e.target;
+
         setFormData({
             ...formData,
             [name]: value
         });
+
+
+        if (name === 'country') {
+            handleCountryChange(value);
+        }
     };
+
+
+    const handleCountryChange = (country) => {
+        if (country === 'Canada') {
+            setProvinces([
+                'Alberta', 'British Columbia', 'Manitoba', 'New Brunswick', 'Newfoundland and Labrador',
+                'Nova Scotia', 'Ontario', 'Prince Edward Island', 'Quebec', 'Saskatchewan'
+            ]);
+        } else if (country === 'USA') {
+            setProvinces([
+                'California', 'Texas', 'Florida', 'New York', 'Illinois', 'Pennsylvania'
+            ]);
+        } else {
+            setProvinces([]);
+        }
+    };
+
 
 
 
     const handleSignup = async (e) => {
         e.preventDefault();
+
+
+        if (!formData.firstName || !formData.lastName || !formData.email || !formData.password || !formData.mobilenumber || !formData.gender || !formData.role || !formData.address || !formData.country) {
+            setError('Please fill all required fields.');
+            return;
+        }
+
+
+        const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+        if (!emailPattern.test(formData.email)) {
+            setError('Please enter a valid email address.');
+            return;
+        }
+
+
+        if (formData.password.length < 6) {
+            setError('Password should be at least 6 characters long.');
+            return;
+        }
+
+        setError('');
 
         try {
             const userCredential = await createUserWithEmailAndPassword(auth, formData.email, formData.password);
@@ -63,154 +114,181 @@ const SignupPage = () => {
         }
     };
 
-   
+    const handleLoginClick = () => {
+        navigate('/login');
+      };
+
+
+
 
 
     return (
         <div className="signup-container">
-    <div className="form-wrapper">
-        <h1 className='text-center'>Sign Up</h1>
+            <div className="form-wrapper">
+                <h1 className='text-center'>Sign Up</h1>
 
-        <form onSubmit={handleSignup}>
-            <div className='form-group'>
-                <label>First Name</label>
-                <input
-                    type='text'
-                    className='form-control'
-                    name='firstName'
-                    value={formData.firstName}
-                    onChange={handleInputChange}
-                    required
-                />
-            </div>
-            <div className='form-group'>
-                <label>Last Name</label>
-                <input
-                    type='text'
-                    className='form-control'
-                    name='lastName'
-                    value={formData.lastName}
-                    onChange={handleInputChange}
-                    required
-                />
-            </div>
-            <div className='form-group'>
-                <label>Email</label>
-                <input
-                    type='email'
-                    className='form-control'
-                    name='email'
-                    value={formData.email}
-                    onChange={handleInputChange}
-                    required
-                />
-            </div>
-            <div className='form-group'>
-                <label>Mobile Number</label>
-                <input
-                    type='number'
-                    className='form-control'
-                    name='mobilenumber'
-                    value={formData.mobilenumber}
-                    onChange={handleInputChange}
-                    required
-                />
-            </div>
-            <div className='form-group'>
-                <label>Password</label>
-                <input
-                    type='password'
-                    className='form-control'
-                    name='password'
-                    value={formData.password}
-                    onChange={handleInputChange}
-                    required
-                />
-            </div>
+                <form onSubmit={handleSignup}>
+                    <div className='form-group'>
+                        <label>First Name</label>
+                        <input
+                            type='text'
+                            className='form-control'
+                            name='firstName'
+                            value={formData.firstName}
+                            onChange={handleInputChange}
+                            required
+                        />
+                    </div>
+                    <div className='form-group'>
+                        <label>Last Name</label>
+                        <input
+                            type='text'
+                            className='form-control'
+                            name='lastName'
+                            value={formData.lastName}
+                            onChange={handleInputChange}
+                            required
+                        />
+                    </div>
+                    <div className='form-group'>
+                        <label>Email</label>
+                        <input
+                            type='email'
+                            className='form-control'
+                            name='email'
+                            value={formData.email}
+                            onChange={handleInputChange}
+                            required
+                        />
+                    </div>
+                    <div className='form-group'>
+                        <label>Mobile Number</label>
+                        <input
+                            type='number'
+                            className='form-control'
+                            name='mobilenumber'
+                            value={formData.mobilenumber}
+                            onChange={handleInputChange}
+                            required
+                        />
+                    </div>
+                    <div className='form-group'>
+                        <label>Password</label>
+                        <input
+                            type='password'
+                            className='form-control'
+                            name='password'
+                            value={formData.password}
+                            onChange={handleInputChange}
+                            required
+                        />
+                    </div>
 
-            <div className="form-group">
-                <label>Gender</label>
-                <div className="gender-group">
-                    <input
-                        type="radio"
-                        id="male"
-                        name="gender"
-                        value="Male"
-                        checked={formData.gender === 'Male'}
-                        onChange={handleInputChange}
-                    />
-                    <label htmlFor="male">Male</label>
-                    <input
-                        type="radio"
-                        id="female"
-                        name="gender"
-                        value="Female"
-                        checked={formData.gender === 'Female'}
-                        onChange={handleInputChange}
-                    />
-                    <label htmlFor="female">Female</label>
-                    <input
-                        type="radio"
-                        id="custom"
-                        name="gender"
-                        value="Custom"
-                        checked={formData.gender === 'Custom'}
-                        onChange={handleInputChange}
-                    />
-                    <label htmlFor="custom">Others</label>
-                </div>
-            </div>
+                    <div className="form-group">
+                        <label>Gender</label>
+                        <div className="gender-group">
+                            <input
+                                type="radio"
+                                id="male"
+                                name="gender"
+                                value="Male"
+                                checked={formData.gender === 'Male'}
+                                onChange={handleInputChange}
+                            />
+                            <label htmlFor="male">Male</label>
+                            <input
+                                type="radio"
+                                id="female"
+                                name="gender"
+                                value="Female"
+                                checked={formData.gender === 'Female'}
+                                onChange={handleInputChange}
+                            />
+                            <label htmlFor="female">Female</label>
+                            <input
+                                type="radio"
+                                id="custom"
+                                name="gender"
+                                value="Custom"
+                                checked={formData.gender === 'Custom'}
+                                onChange={handleInputChange}
+                            />
+                            <label htmlFor="custom">Others</label>
+                        </div>
+                    </div>
 
-            <div className='form-group'>
-                <label>Role</label>
-                <select
-                    className='form-control'
-                    name='role'
-                    value={formData.role}
-                    onChange={handleInputChange}
-                    required>
-                    <option value=''>Select</option>
-                    <option value='standarduser'>Standard User</option>
-                    <option value='moderator'>Moderator</option>
-                    <option value='admin'>Admin</option>
-                </select>
-            </div>
+                    <div className='form-group'>
+                        <label>Role</label>
+                        <select
+                            className='form-control'
+                            name='role'
+                            value={formData.role}
+                            onChange={handleInputChange}
+                            required>
+                            <option value=''>Select</option>
+                            <option value='standarduser'>Standard User</option>
+                            <option value='moderator'>Moderator</option>
+                            <option value='admin'>Admin</option>
+                        </select>
+                    </div>
 
-            <div className='form-group'>
-                <label>Address</label>
-                <input
-                    type='text'
-                    className='form-control'    
-                    name='address'
-                    value={formData.address}
-                    onChange={handleInputChange}
-                    required
-                />
-            </div>
-            <div className='form-group'>
-                <label>Country</label>  
-                <select
-                    className='form-control'
-                    name='countre'
-                    value={formData.role}
-                    onChange={handleInputChange}
-                    required>
-                    <option value=''>Select</option>
-                    <option value='standarduser'>Canada</option>
-                    <option value='moderator'>USA</option>
-                </select>
-            </div>
-        
+                    <div className='form-group'>
+                        <label>Address</label>
+                        <input
+                            type='text'
+                            className='form-control'
+                            name='address'
+                            value={formData.address}
+                            onChange={handleInputChange}
+                            required
+                        />
+                    </div>
 
-            <div className="button-group-signup">
-                <button type='submit' className='signup-btn-custom'>
-                    Sign Up
-                </button>
-               
-                </div>
-        </form>
-    </div>
-</div>
+                    <div className="form-group">
+                        <label>Country</label>
+                        <select
+                            className="form-control"
+                            name="country"
+                            value={formData.country}
+                            onChange={handleInputChange}
+                            required
+                        >
+                            <option value="">Select</option>
+                            <option value="Canada">Canada</option>
+                            <option value="USA">USA</option>
+                        </select>
+                    </div>
+
+                    {formData.country && (
+                        <div className="form-group">
+                            <label>Province</label>
+                            <select
+                                className="form-control"
+                                name="province"
+                                value={formData.province}
+                                onChange={handleInputChange}
+                                required
+                            >
+                                <option value="">Select</option>
+                                {provinces.map((province, index) => (
+                                    <option key={index} value={province}>
+                                        {province}
+                                    </option>
+                                ))}
+                            </select>
+                        </div>
+                    )}
+
+                    <div className="button-group-signup">
+                        <button type='submit' className='signup-btn-custom'>
+                            Sign Up
+                        </button>
+                        <button className="btn btn-custom" onClick={handleLoginClick}>Login</button>
+
+                    </div>
+                </form>
+            </div>
+        </div>
 
 
 
