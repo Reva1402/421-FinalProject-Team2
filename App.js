bb47993100992c4439f249ddbcde6f566f3781a3
Reva1402 <148901721+Reva1402@users.noreply.github.com>
2024-11-05 09:27:03 -0500

    Update App.js

--- App.js
+++ App.js
@@ -2,6 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import WelcomePage from './WelcomePage';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoginPage from './LoginPage';
+import HomePage from './UserHomePage';
+import AdminDashboard from './AdminDashboard';
+import UserManagement from './UserManagement';
+import CreateEvent from './CreateEvent';
 
 function App() {
   return (
@@ -9,6 +14,11 @@ function App() {
       <Router>
       <Routes>
       <Route path="/" element={<WelcomePage />} />
+      <Route path="login" element={<LoginPage />} />
+      <Route path="userhomepage" element={<HomePage />} />
+      <Route path="admin" element={<AdminDashboard />} />
+      <Route path="usermanagement" element={<UserManagement />} />
+      <Route path="createevent" element={<CreateEvent />} />
       </Routes>
       </Router>
     </div>
